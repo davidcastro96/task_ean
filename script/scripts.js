b11ea12f5658acbe0105b7b0571e4db98052e8c7
davidcastro96 <davidcastro0796@outlook.es>
2022-11-12 18:12:18 -0500

    página terminada

--- script/scripts.js
+++ script/scripts.js
@@ -1,44 +1,105 @@
-function crearTarea() {}
+let form = document.getElementById("form");
+/* datos para las tareas */
+let titulo_nueva_tarea = document.getElementById("titulo_nueva_tarea");
+let detalle_nueva_tarea = document.getElementById("detalle_nueva_tarea");
+let tipo_tarea = document.getElementById("select_tipo_nueva_tarea");
+let tiempo_conclucion = document.getElementById("conclucion_nueva_tarea");
+/* -- */
+let msg = document.getElementById("msg");
+let tasks = document.getElementById("tasks");
+let add = document.getElementById("add");
 
-function completarTarea() {}
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  formValidation();
+});
 
-function actualizarTarea() {}
+let formValidation = () => {
+  if (
+    titulo_nueva_tarea.value === "" ||
+    detalle_nueva_tarea === "" ||
+    tipo_tarea === "" ||
+    tiempo_conclucion == ""
+  ) {
+    alert("No están los datos completos, ingreselos todos");
+  } else {
+    console.log("success");
+    msg.innerHTML = "";
+    acceptData();
+    add.setAttribute("data-bs-dismiss", "modal");
+    add.click();
 
-function eliminarTarea() {}
+    (() => {
+      add.setAttribute("data-bs-dismiss", "");
+    })();
+  }
+};
 
-fetch("task.json")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (tasks) {
-    let placeholder = document.querySelector("#task_cards");
-    let out = "";
+let data = [{}];
 
-    for (let task of tasks) {
-      out += `
-      <div class="col-md-4">
+let acceptData = () => {
+  data.push({
+    titulo: titulo_nueva_tarea.value,
+    detalle: detalle_nueva_tarea.value,
+    tipo: select_tipo_nueva_tarea.value,
+    conclucion: tiempo_conclucion.value,
+  });
+
+  localStorage.setItem("data", JSON.stringify(data));
+
+  //console.log(data);
+  createTasks();
+};
+
+let createTasks = () => {
+  tasks.innerHTML = "";
+  data.map((x, y) => {
+    return (tasks.innerHTML += `
+    <div class="col-md-4">
       <div class="jumbotron card card-block">
-        <h2 class="text-uppercase">
-        ${task.tipo_tarea}
-        </h2>
-        <h4 class="font-weight-bold">
-          ${task.titulo_tarea}
-        </h4>
+        <span id="id" hidden="hidden">${y}</span>
+        <h2 id="tipo_tarea" class="text-uppercase">${x.tipo}</h2>
+        <h4 id="titulo_tarea" class="font-weight-bold">${x.titulo}</h4>
+        <p id="detalle_tarea">${x.detalle}</p>
+        <h5 class="pb-3 text-right font-weight-light">horas</h5>
+        <span class="pb-3 text-right font-weight-light">${x.conclucion}</span>
         <p>
-        ${task.detalle_tarea}
-        </p>
-        <span class="pb-3 text-right font-weight-light">Duración : ${task.tiempo_conclucion} horas</span>
-        <p>
-          <button id="completar_tarea" onclick="completarTarea();" class="btn btn-primary btn-large m-1"
-            href="#">completar</button>
-          <button id="actualizar_tarea" onclick="actualizarTarea();" class="btn btn-success btn-large m-1"
-            href="#">actualizar</button>
-          <button id="eliminar_tarea" onclick="eliminarTarea();" class="btn btn-danger btn-large m-1"
-            href="#">eliminar</button>
+          <button type="submit" onClick="editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="btn btn-success btn-large m-1">actualizar</button>
+          <button type="button" onClick="deleteTask(this);" class="btn btn-danger btn-large m-1">eliminar</button>
         </p>
       </div>
     </div>
-    `;
-    }
-    placeholder.innerHTML = out;
+    `);
   });
+  resetForm();
+};
+
+let deleteTask = (e) => {
+  e.parentElement.parentElement.remove();
+  data.splice(e.parentElement.parentElement.id, 1);
+  localStorage.setItem("data", JSON.stringify(data));
+  console.log(data);
+};
+
+let editTask = (e) => {
+  let selectedTask = e.parentElement.parentElement;
+
+  select_tipo_nueva_tarea.value = selectedTask.children[1].innerHTML;
+  titulo_nueva_tarea.value = selectedTask.children[2].innerHTML;
+  detalle_nueva_tarea.value = selectedTask.children[3].innerHTML;
+  tiempo_conclucion.value = selectedTask.children[5].innerHTML;
+
+  deleteTask(e);
+};
+
+let resetForm = () => {
+  titulo_nueva_tarea.value = "";
+  detalle_nueva_tarea.value = "";
+  select_tipo_nueva_tarea.value = "";
+  tiempo_conclucion.value = "";
+};
+
+(() => {
+  data = JSON.parse(localStorage.getItem("data")) || [];
+  createTasks();
+})();
